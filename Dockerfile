8614482744d05e0c138382c5f37000de57d6b8d3
Dhi Aurrahman <dio@tetrate.io>
2019-09-01 13:28:15 +0700

    Make the cluster dynamic as well
    Signed-off-by: Dhi Aurrahman <dio@tetrate.io>

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,11 @@ FROM envoyproxy/envoy-alpine:v1.11.1
 RUN mkdir -p /etc/xds
 RUN echo "lds1.yaml" > /etc/xds/current
 COPY ./lds1.yaml /etc/xds/lds-active.yaml
+COPY ./cds1.yaml /etc/xds/cds-active.yaml
+
 COPY ./lds1.yaml /etc/xds/lds1.yaml
 COPY ./lds2.yaml /etc/xds/lds2.yaml
+COPY ./cds1.yaml /etc/xds/cds1.yaml
+COPY ./cds2.yaml /etc/xds/cds2.yaml
 
 COPY ./toggle.sh /etc/toggle.sh
--- toggle.sh
+++ toggle.sh
@@ -3,12 +3,15 @@
 current=$(cat /etc/xds/current)
 if [ "$current" == "lds1.yaml" ]; then
     cat /etc/xds/lds2.yaml > /tmp/lds.yaml
+    cat /etc/xds/cds2.yaml > /tmp/cds.yaml
     echo "lds2.yaml" > /etc/xds/current
     echo "updated to lds2.yaml"
 else
     cat /etc/xds/lds1.yaml > /tmp/lds.yaml
+    cat /etc/xds/cds1.yaml > /tmp/cds.yaml
     echo "lds1.yaml" > /etc/xds/current
     echo "updated to lds1.yaml"
 fi
 
 mv /tmp/lds.yaml /etc/xds/lds-active.yaml
+mv /tmp/cds.yaml /etc/xds/cds-active.yaml
